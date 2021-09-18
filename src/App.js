a0089103e373a99c7b484d9a7d6c11e670e2867a
DallogFheir <dallog.fheir@gmail.com>
2021-09-18 04:44:04 +0200

    title screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import "./App.css";
+
 function App() {
-  return <div>Cluedo Solver</div>;
+  return (
+    <div class="container">
+      <div class="title-screen">
+        <p className="title">Cluedo Solver</p>
+        <p>Wpisz imiona graczy:</p>
+        <div>player1</div>
+        <div>player2</div>
+        <div>player3</div>
+        <div>player4</div>
+        <div>player5</div>
+        <div>player6</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
